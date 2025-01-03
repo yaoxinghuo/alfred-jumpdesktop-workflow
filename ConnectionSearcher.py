81177f78d9701d0a462de182238b65fe1f394647
zgs225 <zgs225@gmail.com>
2025-01-03 08:34:34 +0800

    fix: Fixed the issue of not working on the new version of Jump Desktop.

--- ConnectionSearcher.py
+++ ConnectionSearcher.py
@@ -1,28 +1,59 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import json
-import sys
 import glob
+import json
+import logging
+import os
 import plistlib
-from workflow import Workflow, ICON_WARNING, MATCH_SUBSTRING
+import sys
+
+from workflow import ICON_WARNING, MATCH_SUBSTRING, Workflow3
 
 
 def read_connections():
     # Read preferences file
-    preferences_path = os.path.join(os.environ["HOME"], "Library", "Containers", "com.p5sys.jump.mac.viewer", "Data", "Library", "Preferences", "com.p5sys.jump.mac.viewer.plist")
+    preferences_path = os.path.join(
+        os.environ["HOME"],
+        "Library",
+        "Containers",
+        "com.p5sys.jump.mac.viewer",
+        "Data",
+        "Library",
+        "Preferences",
+        "com.p5sys.jump.mac.viewer.plist",
+    )
     # if preference path not exists, try this location:
     if not os.path.isfile(preferences_path):
-        preferences_path = os.path.join(os.environ["HOME"], "Library", "Preferences", "com.p5sys.jump.mac.viewer.web.plist")
+        preferences_path = os.path.join(
+            os.environ["HOME"],
+            "Library",
+            "Preferences",
+            "com.p5sys.jump.mac.viewer.web.plist",
+        )
+
+    # try setapp jump desktop
+    if not os.path.isfile(preferences_path):
+        preferences_path = os.path.join(
+            os.environ["HOME"],
+            "Library",
+            "Preferences",
+            "com.p5sys.jumpdesktop-setapp.plist",
+        )
 
     connections = []
-    with open(preferences_path, 'rb') as fp:
+    with open(preferences_path, "rb") as fp:
         plist = plistlib.load(fp)
         # Extract profile data from plist
-        connection_path = plist.get('path where JSON .jump files are stored')
-        if connection_path.startswith('~'):
-            connection_path = os.environ["HOME"] + connection_path[1:]
+        connection_path = plist.get("path where JSON .jump files are stored")
+
+        if connection_path is None:
+            connection_path = "~/Documents/JumpDesktop/Viewer/Servers"
+
+        connection_path = os.path.normpath(os.path.expanduser(connection_path))
+
+        logging.info("connection_path: {}", connection_path)
+
         jumps = glob.glob(connection_path + "/Computer - *.jump")
         connections = []
         for jump in jumps:
@@ -31,66 +62,79 @@ def read_connections():
             f.close()
             dict_content = json.loads(json_content)
             icon = None
-            if dict_content['Icon']:
-                icon = "/Applications/Jump Desktop.app/Contents/Resources/%s.png" % dict_content['Icon']
-            username = ''
+            if dict_content["Icon"]:
+                icon = (
+                    "/Applications/Jump Desktop.app/Contents/Resources/%s.png"
+                    % dict_content["Icon"]
+                )
+            username = ""
             if "Username" in dict_content:
-                username = dict_content['Username']
-            command = 'jump://?protocol=%s&host=%s&username=%s' % \
-                      (protocol_switch(dict_content['ProtocolTypeCode']), dict_content['TcpHostName'], username)
+                username = dict_content["Username"]
+            command = "jump://?protocol=%s&host=%s&username=%s" % (
+                protocol_switch(dict_content["ProtocolTypeCode"]),
+                dict_content["TcpHostName"],
+                username,
+            )
 
-            connections.append({
-                'name': dict_content['DisplayName'],
-                'command': command,
-                'path': jump,
-                'icon': icon,
-                'tags': dict_content['Tags']
-            })
+            connections.append(
+                {
+                    "name": dict_content["DisplayName"],
+                    "command": command,
+                    "path": jump,
+                    "icon": icon,
+                    "tags": dict_content["Tags"],
+                }
+            )
 
     return connections
 
 
 def protocol_switch(x):
     return {
-        0: 'rdp',
-        1: 'vnc',
-        2: 'fluid',
+        0: "rdp",
+        1: "vnc",
+        2: "fluid",
     }.get(x, 0)
 
 
 def filter_key_for_connection(connection):
-    return connection['name'] + ' ' + str(connection['tags'])
+    return connection["name"] + " " + str(connection["tags"])
 
 
 def sort_key_for_connection(connection):
-    return connection['name']
+    return connection["name"]
 
 
-def main(wf):
-    connections = wf.cached_data('connections', read_connections, max_age=30)
+def main(wf: Workflow3):
+    connections = wf.cached_data("connections", read_connections, max_age=30)
 
     # Query argument Ensure `query` is initialised
     query = None
     if len(wf.args):
         query = wf.args[0]
 
-    connections = wf.filter(query, connections, filter_key_for_connection, match_on=MATCH_SUBSTRING)
+    connections = wf.filter(
+        query, connections, filter_key_for_connection, match_on=MATCH_SUBSTRING
+    )
 
     if not connections:
-        wf.add_item('No connection matches', icon=ICON_WARNING)
+        wf.add_item("No connection matches", icon=ICON_WARNING)
 
     connections.sort(key=sort_key_for_connection)
     for connection in connections:
-        wf.add_item(title=connection['name'],
-                    subtitle=((str(connection['tags']) + ' ') if connection['tags'] else '') + connection['command'],
-                    arg=connection['path'],
-                    valid=True,
-                    icon=connection['icon'])
+        wf.add_item(
+            title=connection["name"],
+            subtitle=((str(connection["tags"]) + " ") if connection["tags"] else "")
+            + connection["command"],
+            arg=connection["path"],
+            valid=True,
+            icon=connection["icon"],
+        )
 
     wf.send_feedback()
     return
 
 
-if __name__ == u"__main__":
-    wf = Workflow()
+if __name__ == "__main__":
+    wf = Workflow3()
     sys.exit(wf.run(main))
